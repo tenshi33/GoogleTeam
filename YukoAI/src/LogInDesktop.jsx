8c71b47b7e8293b7ee3ca5fec3874ee6f9443bc6
faith-clm <faithcalma07@gmail.com>
2024-11-15 15:34:25 +0800

    added log in desktop version

---
+++ YukoAI/src/LogInDesktop.jsx
@@ -0,0 +1,30 @@
+
+function LogInDesktop() {
+    return(
+        <>
+            <section className="log-in-section" id="Log-In">
+                <img className="desktop-bg" src="Desktop without ic.png" alt="Background Image"/>
+
+                <div className="log-in">
+                    <h1 className="log-in-text">Log In</h1>
+                    <input className="email-address-button" type="email" placeholder="Email Address"></input>
+                    <input className="password-button" type="password" placeholder="Password"></input>
+                    <a className="forgot-pass-text" href="#" target="_self">Forgot Password?</a>
+                    <button className="log-in-button">Log In</button>
+                    <div className="no-account">
+                        <p className="no-account-text">Don’t have an account yet?</p>
+                        <a className="sign-up-button" href="#">Sign Up</a>
+                    </div>
+                </div>
+
+                <div className="yuko-logo">
+                    <img className="logo" src="YUKO-LOGO-FULL.png" alt="Yuko Logo"/>
+                </div>
+            </section>
+            
+        </>
+        
+    );
+}
+
+export default LogInDesktop
