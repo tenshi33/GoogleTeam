6a00164a44f1379e941cfdaae9dcba2e445fc80a
Yohan <yohancaballeroo@gmail.com>
2024-11-18 13:37:07 +0800

    Faith Updt

--- Yuko/src/components/Login.jsx
+++ Yuko/src/components/Login.jsx
@@ -62,44 +62,54 @@ function LogInDesktop() {
 
   return (
     <>
-      <section className="log-in-section" id="Log-In">
-      <div className="yuko-logo">
-      <img className="logo" src="./yuko_logo2.png" alt="Yuko Logo" />
-    </div>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=mail" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=lock" />
+    <div className="log-in-container">
+      <img className="yuko-logo" src="yuko_logo_full.png" alt="Yuko Logo"/>
 
-    <img className="desktop-bg" src="./background.png" alt="Background Image" />
+      
+      <form action="#" className="log-in-form">
+        <h2 className="login-title">Log In</h2>
 
-        <div className="log-in">
-          <h1 className="log-in-text">Log In</h1>
-          <input
-            className="email-address-button"
-            type="email"
-            placeholder="Email Address"
+        <div className="input-wrapper">
+          <input 
+            className="input-field" 
+            type="email" 
+            placeholder="Email Address" 
             value={email}
             onChange={(e) => setEmail(e.target.value)} // Handle email input change
-          required
+            required
           />
-          <input
-            className="password-button"
-            type="password"
-            placeholder="Password"
+          <i className="material-symbols-outlined">mail</i>
+        </div>
+
+        <div className="input-wrapper">
+          <input 
+            className="input-field" 
+            type="password" 
+            placeholder="Password" 
             value={password}
             onChange={(e) => setPassword(e.target.value)} // Handle password input change
-          required
+            required
           />
-          <p className="recver">
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                setPasswordModalVisible(true);
-              }}
-            >
+          <i className="material-symbols-rounded">lock</i>
+          <button className="show-pass">Show</button>
+        </div>
+        
+        <div className="forgot-password-wrapper">
+          <a 
+            className="forgot-pass-link" 
+            href="#" 
+            target="_self" 
+            onClick={(e) => {
+              e.preventDefault();
+              setPasswordModalVisible(true);
+            }}>
               Forgot Password?
-            </a>
-          </p>
-          
-          {isPasswordModalVisible && (
+          </a>
+        </div>
+
+        {isPasswordModalVisible && (
             <div id="recoverPasswordModal">
               <input
                 type="email"
@@ -121,22 +131,19 @@ function LogInDesktop() {
             </div>
           )}
 
-          <button className="log-in-button" onClick={handleLogin}>
-            Log In
-          </button>
+        <button className="log-in-button" onClick={handleLogin}>Log In</button>
 
-          {errorMessage && (
+        {errorMessage && (
             <p className="error-message" style={{ color: "red" }}>
               {errorMessage}
             </p>
           )}
 
-          <p className="no-account-text">
-            Don’t have an account yet?
-            <a className="sign-up-button" href="#">Sign Up</a>
-          </p>
-        </div>
-      </section>
+        <p className="no-account-text">Don't have an account yet?
+          <a className="sign-up-button" href="#">Sign Up</a>
+        </p>
+      </form>
+    </div>
     </>
   );
 }
