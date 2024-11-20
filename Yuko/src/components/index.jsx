b00cf5019a5a942d017240eee67034da57464fbe
faith-clm <faithcalma07@gmail.com>
2024-11-20 19:43:53 +0800

    landing page dev html

--- Yuko/src/components/index.jsx
+++ Yuko/src/components/index.jsx
@@ -7,20 +7,35 @@ function Landing() {
         <>
             <div className='landing-header'>
                 <img className='header-yuko-logo' src='#' alt='Yuko Logo'/>
-                <a>Home</a>
+                <a className='header-home'>Home</a>
                 <p className='header-dividers'>|</p>
-                <a>Features</a>
+                <a className='header-features'>Features</a>
                 <p className='header-divider'>|</p>
-                <a>Developers</a>
+                <a className='header-developers'>Developers</a>
                 <p className='header-divider'>|</p>
-                <a>Our Services</a>
+                <a className='header-services'>Our Services</a>
                 <p className='header-divider'>|</p>
-                <a>Contacts</a>
+                <a className='header-contacts'>Contacts</a>
             </div>
-        
-            <h1>Landing</h1>
-            <Link to="/Login" className="log-in-button">Log In</Link>
-            <div className='Header'></div>
+
+            <section id='home'>
+                <h1 className='home-yuko-title'>Yuko</h1>
+                <p className='home-yuko-description'>Decription</p>
+                <Link to="/Login" className="log-in-button">Log In</Link>
+            </section>
+
+            <section id='features'>
+                <h1>FEATURES</h1>
+                <div></div>
+            </section>
+
+            <section id='developers'>
+                <h1>DEVELOPERS</h1>
+            </section>
+
+            <section id='services'></section>
+
+            <section id='contacts'></section>
         </>
     )
 }
