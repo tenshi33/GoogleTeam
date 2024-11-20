84537dcd84c94b1ed3d22497c443bc3c56902031
faith-clm <faithcalma07@gmail.com>
2024-11-20 20:00:45 +0800

    landing page dev html only

--- Yuko/src/components/index.jsx
+++ Yuko/src/components/index.jsx
@@ -28,6 +28,7 @@ function Landing() {
                 <div className='features-first-section'>
                     <h1>FEATURES</h1>
                     <p>Some Description</p>
+                    <img>Yuko Chatbot Pictures in Phone</img>
                 </div>
                 <div className='features-scroll-section'>
                     <div className='first-feature'>
