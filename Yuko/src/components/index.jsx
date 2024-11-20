9338f10f17ca55890173e63764c4108877a021cf
Yohan <yohancaballeroo@gmail.com>
2024-11-20 13:46:11 +0800

    Log in button function

--- Yuko/src/components/index.jsx
+++ Yuko/src/components/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/index.css'
 
 function Landing() {
     return (
         <>
         <h1>Landing</h1>
-        <button>Log In</button>
+        <Link to="/Login" className="log-in-button">Log In</Link>
         </>
     )
 }
