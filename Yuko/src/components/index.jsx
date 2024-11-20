a27763d499e89e2f1ab1e443bdfbde6b61406f83
Yohan <yohancaballeroo@gmail.com>
2024-11-20 13:14:07 +0800

    Landing Page

---
+++ Yuko/src/components/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../styles/index.css'
+
+function Landing() {
+    return (
+        <>
+        <h1>Landing</h1>
+        </>
+    )
+}
+
+export default Landing;
--- Yuko/src/main.jsx
+++ Yuko/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login.jsx';
-import Register from './components/Register.jsx';
-import Yuko from './components/Yuko.jsx';
+import Landing from './components/index'
+import Login from './components/Login';
+import Register from './components/Register';
+import Yuko from './components/Yuko';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/yuko" element={<Yuko />} />
       </Routes>
