d5f3162d382fee1533f243e73409f095c19c5c2b
Faith <faithcalma07@gmail.com>
2024-11-25 09:56:14 +0800

    fixed body in all pages

--- Yuko/src/components/Login.jsx
+++ Yuko/src/components/Login.jsx
@@ -112,6 +112,8 @@ function LogInDesktop() {
     <>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=mail" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=lock" />
+
+      
       <div className="log-in-container" id='log-in'>
         <img className="bg-image" src="background.png" alt="Background Image of Yuko"/>
         <img className="yuko-logo" src="yuko_logo_full.png" alt="Yuko Logo" />
