534abdad7452ba22cfd775271b75eacc9401d32b
Yohan <yohancaballeroo@gmail.com>
2024-12-05 00:10:30 +0800

    Disabled Yuko

---
+++ Yuko/src/components/error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+    return (
+      <>
+        <h1>Yuko will be back soon.</h1>
+      </>
+    );
+  };
+
+export default Error;
--- Yuko/src/main.jsx
+++ Yuko/src/main.jsx
@@ -5,12 +5,13 @@ import Landing from './components/index'
 import Login from './components/Login';
 import Register from './components/Register';
 import Yuko from './components/Yuko';
+import Error from './components/error';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Error />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/yuko" element={<Yuko />} />
